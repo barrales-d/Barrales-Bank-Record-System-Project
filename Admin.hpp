b0a9b94da062b219d1a769885ef9d579b977120c
barrales-d <diego.barrales@csu.fullerton.edu>
2022-07-13 11:44:12 -0700

    Creating Admin.hpp for the Admin class that will be able to create, delete, modify, and view all accounts

---
+++ Admin.hpp
@@ -0,0 +1,50 @@
+#ifndef __ADMIN_HPP__
+#define __ADMIN_HPP__
+
+#include <string>
+#include <map>
+#include "Users.hpp"
+
+
+//	tag to help find a specific location in file? 
+const std::string ACCOUNT_TAG = "ACCOUNTS";
+const int ZERO_CHAR_ASCII = 48;
+
+class Admin : protected Users {
+private:
+	// map username to number of accounts that person has
+	std::map<std::string, int> m_userDatabase;
+public:
+	//	Sign in constructor uses the ADMIN.txt to fill the m_userDatabase;
+	Admin(const std::string& username, const std::string& password) : User(username, password) {
+		m_FileManager.open(m_fileName, std::fstream::in);
+		std::string data;
+		//read file until you find the tag ACCOUNTS
+		while (std::getline(m_FileManager, data) && data != ACCOUNT_TAG) {}
+
+		//	get information from file about all the users 
+		while (std::getline(m_FileManager, data)) {
+			//	gets the last char from the string data, then subtracts 48 from it to get the real number
+			int number_of_accounts = data.at(data.length() - 1) - ZERO_CHAR_ASCII;
+			//	resizes the string dat to only have the username, without the "space + number"
+			//	i.e. username 3 = username
+			data.resize(data.length() - 2);
+			//	puts data into the database;
+			m_userDatabase[data] = number_of_accounts;
+		}
+		//closes file
+		m_FileManager.close();
+	}
+	~Admin();
+	//const std::map<std::string, int>& database() { return m_userDatabase; } - unsure if I need it outside of this class
+
+	//	Admins have the power to create accounts for a user, delete account, show all accounts, modify account? 
+
+	//	Create Account for this username
+	int createAccount(const std::string& username, const std::string& password, const std::string& accountType, int startingAmount);
+	//	Delete Account for this username
+	//	Delete Account(const std::string& username, int accountNumber);
+
+
+};
+#endif //	!__Admin_HPP__ 
--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#include "Users.hpp"
+
 //	trying to changing this and add to github
 //	want to test this change as well :)
 int main() {
